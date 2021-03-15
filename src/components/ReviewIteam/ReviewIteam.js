fba73ace87f99665ef8e2476eca630717f43b08e
Zahidapu <37400394+Zahidapu@users.noreply.github.com>
2021-03-16 01:11:48 +0600

    css added to reviewiteams

---
+++ src/components/ReviewIteam/ReviewIteam.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ReviewIteam.css'
+
+const ReviewIteam = (props) => {
+    console.log(props);
+    const {name,quantity}=props.product;
+    return (
+        <div className="review-iteam">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity:{quantity}</p>
+            <br/>
+            <button className="main-button">Remove</button>
+        </div>
+    );
+};
+
+export default ReviewIteam;
